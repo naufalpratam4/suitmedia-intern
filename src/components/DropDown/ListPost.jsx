043e51221e036dd2283161a1a6403517cb45c702
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-12-23 19:50:53 +0700

    update API

--- src/components/DropDown/ListPost.jsx
+++ src/components/DropDown/ListPost.jsx
@@ -15,7 +15,7 @@ function ListPost({ itemsPerPage, onItemsPerPageChange }) {
         >
           <option value={10}>10</option>
           <option value={20}>20</option>
-          <option value={30}>30</option>
+          <option value={50}>50</option>
         </Form.Control>
       </Col>
     </Form.Group>
