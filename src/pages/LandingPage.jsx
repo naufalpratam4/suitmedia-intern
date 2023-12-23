1c00bfdc6354127b80a2f3be7a229c43151e6df0
naufalpratam4 <naufal.d.pratama@gmail.com>
2023-12-23 19:44:58 +0700

    update API

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -10,7 +10,9 @@ function LandingPage() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("/public/APICard.json");
+        const response = await axios.get(
+          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+        );
         setCoinsData(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
